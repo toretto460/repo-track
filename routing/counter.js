13cee3b7225e21ec479298ed98ca3e5627701c29
toretto460 <toretto460@gmail.com>
2013-04-01 22:37:25 +0200

    Fixed counter controller

--- routing/counter.js
+++ routing/counter.js
@@ -7,19 +7,30 @@ module.exports = function(app) {
     var repo_id = req.params.repoid;
     var cookie = req.cookies.repo_track;
 
-    if( !cookie ){
+    var sendCanvas = function (res, value) {
+      res.cookie('repo_track', { viewed: 1 }, { expires: new Date(Date.now() + (60*60*60)) });
+      canvas = new Canvas(50,20);
+      ctx = canvas.getContext('2d');
+      ctx.font = '20px Verdana';
+      ctx.fillText(value, 0, 15);
+      res.send(canvas.toBuffer());  
+    };  
+
       app.get('redis_client').hexists(repo_id, 'counter', function(err, exists){
-        if ( exists ) {
-          app.get('redis_client').hincrby(repo_id, 'counter', 1, function(err, value) {
-            res.cookie('repo_track', { viewed: 1 }, { expires: new Date(Date.now() + (60*60*60)) });
-            canvas = new Canvas(50,20);
-            ctx = canvas.getContext('2d');
-            ctx.font = '20px Verdana';
-            ctx.fillText(value, 0, 15);
-            res.send(canvas.toBuffer());
-          });
+
+        if ( exists ) { 
+          if ( !cookie ) {    
+            app.get('redis_client').hincrby(repo_id, 'counter', 1, function(err, value) {
+              sendCanvas(res, value);
+            });
+          } else {
+            app.get('redis_client').hget(repo_id, 'counter', function(err, value) {
+              sendCanvas(res, value);
+            });
+          }
+        } else {
+          res.send(404);
         }
       });
-    }
   });
 };
