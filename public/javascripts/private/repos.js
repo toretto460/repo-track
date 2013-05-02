70268e4dda5698c037c1b3084d98afa37a391bb2
toretto460 <toretto460@gmail.com>
2013-05-02 22:07:59 +0200

    Layout and repo list

--- public/javascripts/private/repos.js
+++ public/javascripts/private/repos.js
@@ -8,30 +8,47 @@ $(document).ready(function(){
           	repoLi = document.createElement('li');
           	repoLi.appendChild(document.createTextNode(repo.name));
           	repoLi.style = "display: none;";
-            repoLi.class = "repo";
+            repoLi.className = "repo";
             repoLi.setAttribute('data-name', repo.name);
+            repoLi.setAttribute('data-repo', repo.id);
 
           	$('#repo-list').append(repoLi);
-            callback(repoLi);
-            setTimeout(function(){
-              $('#' + repo.id).fadeIn('slow');  
-            },10);
+            callback(repoLi, repo);
+
           };
 
           $.each(data, function(){
-          	createRepoElement(this, function(repo){
+          	createRepoElement(this, function(repoLi, repo){
+                var getCounterSnippet = document.createElement('span');
+                getCounterSnippet.innerText = "get Snippet";
+                getCounterSnippet.className = "get-snippet to-right";
+                getCounterSnippet.setAttribute('data-repo', repo.id);
+                $(getCounterSnippet).click(function(el){
+                    alert('clicked get snippet for repo ' + repo.id);
+                });
+                var counter = document.createElement('span');
+                counter.className = "counter";
+                counter.setAttribute('data-repo', repo.id);
+                $.get('/counter/' + repo.id, function(data){
+                    counter.innerText = data.conter;
+                });
+
+                $(repoLi).append(getCounterSnippet);
+                $(repoLi).fadeIn('slow');
               
             });
 
-          });
+          $('.bubblingG').hide();
 
-          $('#repo-list li').click(function(){
-
-            $.post('register', {repo_name: this.getAttribute('data-name')}, function(data){
-              console.log(data);
-            });
-              
           });
+//
+//          $('#repo-list li').click(function(){
+//
+//            $.post('register', {repo_name: this.getAttribute('data-name')}, function(data){
+//              console.log(data);
+//            });
+//
+//          });
         }
       });
     });
